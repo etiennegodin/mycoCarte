c25e773788d8648cd6fc26e95d1128328d0259be
etiennegodin <etiennegodin@duck.com>
2025-06-11 17:29:31 -0400

    notebooks

--- scripts/preprocessData.py
+++ scripts/preprocessData.py
@@ -4,15 +4,6 @@ import geopandas as gpd
 from mycoCarte import geoUtils
 from mycoCarte.dataPreprocessing import foretOuverte, occurences, bioclim, bias, mergeDataset
 
-grid = geoUtils.clusterGrid(clusters= 6, overwrite= False)
-grid_df = grid.drop(['geometry'], axis = 1)
-foretOuverte_df = foretOuverte.preprocessData(overwrite= False)
-bioclim_df = bioclim.preprocessData()
-bias_df =  bias.readBiasData()
-
-occurences_df = occurences.preprocessData(overwrite = False)
-aggregated_occurences_df = occurences.process_fungi_ecology_index(occurences_df)
-print(aggregated_occurences_df)
 #Classification écologique du territoire québécois
 #block cv based on nature instead of arbitrary 
 #https://www.donneesquebec.ca/recherche/fr/dataset/systeme-hierarchique-de-classification-ecologique-du-territoire
@@ -32,17 +23,29 @@ print(aggregated_occurences_df)
 # Remove occurences in urban area from bias layers  
 #aggregated_occurences_df = occurences.biasFilter(aggregated_occurences_df, bias_df)
 
-datasets_to_merge = { 'foretOuverte': foretOuverte_df,
+# Preprocess Occurences Data
+grid = geoUtils.clusterGrid(clusters= 6, overwrite= False)
+grid_df = grid.drop(['geometry'], axis = 1)
+foretOuverte_df = foretOuverte.preprocessData(overwrite= False)
+bioclim_df = bioclim.preprocessData()
+bias_df =  bias.readBiasData()
+
+occurences_df = occurences.preprocessData(overwrite = False)
+aggregated_occurences_df = occurences.process_fungi_ecology_index(occurences_df)
+print(aggregated_occurences_df)
+
+occurences_datasets_to_merge = { 'foretOuverte': foretOuverte_df,
             'bioclim' :bioclim_df,
             'bias' : bias_df,
             'grid': grid_df
 }
 
-merged_occurences_df = mergeDataset(occurences_df, datasets_to_merge)
-merged_agg_occurences_df = mergeDataset(aggregated_occurences_df, datasets_to_merge)
+merged_occurences_df = mergeDataset(occurences_df, occurences_datasets_to_merge)
+merged_agg_occurences_df = mergeDataset(aggregated_occurences_df, occurences_datasets_to_merge)
 
-merged_occurences_df.to_csv('data/interim/geodata/vector/unifiedPreprocessedData/unifiedPreprocessedData.csv')
-merged_agg_occurences_df.to_csv('data/interim/geodata/vector/unifiedPreprocessedData/aggregatedPreprocessedData.csv')
+
+merged_occurences_df.to_csv('data/interim/model/preprocessedOccurencesData.csv')
+merged_agg_occurences_df.to_csv('data/interim/model/preprocessedAggregatedOccurencesData.csv')
 
 print('-'*100)
 print(merged_occurences_df.shape)
@@ -51,4 +54,19 @@ print(merged_occurences_df.head())
 print('-'*100)
 print(merged_agg_occurences_df.shape)
 print(merged_agg_occurences_df.head())
+
+
+# Create grid data to test 
+grid_datasets_to_merge = { 'foretOuverte': foretOuverte_df,
+            'bioclim' :bioclim_df,
+            'bias' : bias_df
+}
+merge_test_df = mergeDataset(grid_df, grid_datasets_to_merge)
+merge_test_df.to_csv('data/interim/model/testData.csv')
+
+print('-'*100)
+print(merge_test_df.shape)
+print(merge_test_df.head())
+
+
 
